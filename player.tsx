69407abdbc8ea9f54db866153acc7c2e030dfe62
Fantasia_999 <abidshahriyar999@gmail.com>
2026-06-04 20:37:55 +0600

    Add files via upload

---
+++ player.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="player" tilewidth="20" tileheight="26" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <properties>
+  <property name="life" type="bool" value="true"/>
+  <property name="speed" type="float" value="3"/>
+ </properties>
+ <tile id="0">
+  <properties>
+   <property name="life" type="bool" value="true"/>
+   <property name="speed" type="float" value="3"/>
+  </properties>
+  <image source="assets/maps/player.png" width="20" height="26"/>
+ </tile>
+</tileset>
